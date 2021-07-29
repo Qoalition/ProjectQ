0fbf8fcd4df15cbb94496e94f249a9a26a92ec95
Seanathon <seanyalda@gmail.com>
2021-07-29 13:59:22 -0700

    started on questionDetail template

--- client/pages/questionDetail.js
+++ client/pages/questionDetail.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 // Components
 import Header from '../components/header/header'
 import Question from '../components/questions/question/question'
+
+// STILL WORKING ON BREAKING THIS INTO COMPONENTS
 
 const Feed = props => {
     return (
         <div>
             <Header />
-            <Question />
+            <div style={{
+                maxWidth: '48rem',
+                margin: 'auto'
+            }}>
+                <ul>
+                    { /* Tags */ }
+                    <li>Psychology</li>
+                    <li>Behavior</li>
+                </ul>
+                <Question />
+                <hr />
+                100 Answers
+                <hr />
+                {/* Insert answers here */}
+            </div>
         </div>
     );
 };
