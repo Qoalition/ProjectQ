82cf75fb78e6492ef028049ee254973fbab01087
tommyrharper <thomasrobertharper@gmail.com>
2021-08-04 04:15:54 +0100

    Update helpers

--- server/blockchain/helpers.js
+++ server/blockchain/helpers.js
@@ -1,29 +1,60 @@
+const Web3 = require("web3");
+const { abi } = require("../../build/contracts/RootQuestionsContract.json");
 
-const Web3 = require('web3');
-const { abi } = require('../../build/contracts/RootQuestionsContract.json')
-
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
-const contract = new web3.eth.Contract(abi, '0x711d0F250Da1fC44d03A4Be32d1CC9b2ED160CE8');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+const contract = new web3.eth.Contract(
+  abi,
+  "0x711d0F250Da1fC44d03A4Be32d1CC9b2ED160CE8"
+);
 const accounts = web3.eth.getAccounts();
 
 const addQuestion = async (req, res) => {
+  // Change 2 to dynamic value passed in from previous middleware function
+  const questionId = 2;
   try {
-    // Change 2 to dynamic value passed in from previous middleware function
-    const result = await contract.methods.addQuestion(2).call();
-    return res.send(result)
+    const result = await contract.methods.addQuestion(questionId).call();
+    return res.send(result);
   } catch (error) {
-    return res.send({error: error.message})
+    return res.send({ error: error.message });
   }
 };
 
 const getQuestions = async (req, res) => {
   try {
     const result = await contract.methods.getQuestions().call();
-    return res.send(result)
+    return res.send(result);
   } catch (error) {
-    return res.send({error: error.message})
+    return res.send({ error: error.message });
   }
 };
 
+const upVoteQuestionByIndex = async (req, res) => {
+  // Update questionIndex value to be dynamic
+  const questionIndex = 0;
+  try {
+    const result = await contract.methods.upVoteQuestion(questionIndex).call();
+    return res.send(result);
+  } catch (error) {
+    return res.send({ error: error.message });
+  }
+};
 
-module.exports = { addQuestion, getQuestions };
+const downVoteQuestionByIndex = async (req, res) => {
+  // Update questionIndex value to be dynamic
+  const questionIndex = 0;
+  try {
+    const result = await contract.methods
+      .downVoteQuestion(questionIndex)
+      .call();
+    return res.send(result);
+  } catch (error) {
+    return res.send({ error: error.message });
+  }
+};
+
+module.exports = {
+  addQuestion,
+  getQuestions,
+  upVoteQuestionByIndex,
+  downVoteQuestionByIndex,
+};
--- server/server.js
+++ server/server.js
@@ -1,46 +1,45 @@
 "use strict";
-var next = require('next');
-const express = require('express')
-const bodyParser = require('body-parser');
+var next = require("next");
+const express = require("express");
+const bodyParser = require("body-parser");
 // var express = require('express-observer');
-const { contract } = require('./config')
-const Web3 = require('web3');
-var dev = process.env.NODE_ENV !== 'production';
+const { contract } = require("./config");
+const Web3 = require("web3");
+var dev = process.env.NODE_ENV !== "production";
 var app = next({ dev: dev });
-const { abi } = require('../build/contracts/RootQuestionsContract.json')
-const {addQuestion, getQuestions} = require('./blockchain/helpers.js');
+const { abi } = require("../build/contracts/RootQuestionsContract.json");
+const {
+  addQuestion,
+  getQuestions,
+  upVoteQuestionByIndex,
+  downVoteQuestionByIndex,
+} = require("./blockchain/helpers.js");
 
 app.prepare().then(function () {
   var server = express();
 
-  server.use(bodyParser.json())
+  server.use(bodyParser.json());
 
+  server.use("/users", require("./routes/users"));
+  server.use("/questions/", require("./routes/questions"));
+  server.get("/test/addQuestion", addQuestion);
+  server.get("/test/getQuestions", getQuestions);
+  server.get("/test/upVoteQuestionByIndex", upVoteQuestionByIndex);
+  server.get("/test/downVoteQuestionByIndex", downVoteQuestionByIndex);
 
-  server.use('/users', require("./routes/users"));
-  server.use('/questions/', require("./routes/questions"));
-  server.get('/test/addQuestion', addQuestion);
-  server.get('/test/getQuestions', getQuestions)
-  server.get('/test/upVoteQuestion', () => {})
-  server.get('/test/downVoteQuestion', () => {})
-
-
-  server.get('*', function (req, res) {
-    console.log('DEBUG :: ERROR => Recieved an unclaimed request');
+  server.get("*", function (req, res) {
+    console.log("DEBUG :: ERROR => Received an unclaimed request");
     return handle(req, res);
   });
 
   server.use(function (err, req, res, next) {
     console.log("ERROR");
-    return handle(err)
+    return handle(err);
   });
 
   server.listen(5000, function (err) {
-    if (err)
-      throw err;
+    if (err) throw err;
 
-
-
-
-    console.log('Ready on http://localhost:5000');
+    console.log("Ready on http://localhost:5000");
   });
 });
