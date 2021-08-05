f40d54d1f2c35346668fc3841a99a78880830b2c
tommyrharper <thomasrobertharper@gmail.com>
2021-08-05 04:20:17 +0100

    Delete contract.js and config.js

--- server/config.js
+++
@@ -1,8 +0,0 @@
-const Contract = require('./contract.js')
-const { abi } = require('../build/contracts/RootQuestionsContract.json')
-
-const contract = new Contract(abi, process.env.CONTRACT_ADDRESS)
-
-module.exports = {
-  contract
-}
