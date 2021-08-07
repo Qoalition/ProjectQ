151f5b5f1d95d8696e55bb1830f7f380295b77cc
tommyrharper <thomasrobertharper@gmail.com>
2021-08-07 19:50:13 +0100

    Update the deploy contract migration to use infura

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,6 +1,6 @@
 const RootQuestionsContract = artifacts.require("RootQuestionsContract");
 // const AnswerContract = artifacts.require("QuestionAnswer");
 
-module.exports = function (deployer) {
-  deployer.deploy(RootQuestionsContract);
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(RootQuestionsContract, {from: accounts[0]});
 };
