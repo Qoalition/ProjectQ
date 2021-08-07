511979ca5b6bf3ab9442a3f894f1ec5adf8a08c8
tommyrharper <thomasrobertharper@gmail.com>
2021-08-07 19:50:29 +0100

    Update server config

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,5 +1,4 @@
 const RootQuestionsContract = artifacts.require("RootQuestionsContract");
-// const AnswerContract = artifacts.require("QuestionAnswer");
 
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(RootQuestionsContract, {from: accounts[0]});
--- server/config.js
+++ server/config.js
@@ -1,5 +1,8 @@
 const Web3 = require("web3");
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+const provider = new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_URL)
+
+const web3 = new Web3(provider);
 
 module.exports = { web3, DEFAULT_GAS: 2000000 };
